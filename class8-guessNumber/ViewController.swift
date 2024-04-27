1cf01c4938ce39bf97fe2cbcf90a33054e1a4f31
Elsa <elsaliu09@gmail.com>
2024-04-27 16:56:59 +0800

    Initial Commit

---
+++ class8-guessNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  class8-guessNumber
+//
+//  Created by 劉美君 on 2024/4/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ class8-guessNumberUITests/class8_guessNumberUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  class8_guessNumberUITestsLaunchTests.swift
+//  class8-guessNumberUITests
+//
+//  Created by 劉美君 on 2024/4/27.
+//
+
+import XCTest
+
+final class class8_guessNumberUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
